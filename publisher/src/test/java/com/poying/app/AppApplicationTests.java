939723bdb28ce8cadf413d0c9a4262b05b8e26b0
Naruto <2045933252@qq.com>
2024-09-16 02:21:05 +0800

    发送者模块

---
+++ publisher/src/test/java/com/poying/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poying.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
